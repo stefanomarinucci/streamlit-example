8c52b90f7e898668d4242ad382f39008d68fa3fc
Stefano Marinucci <78323127+stefanomarinucci@users.noreply.github.com>
2024-01-29 19:42:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,13 @@ import pandas as pd
 import streamlit as st
 import io
 import os
-#import plotly.express as px
+import plotly.express as px
 from my_functons import italian_date_to_datetime, extract_text_segments, extract_numbers_after_zero, extract_intermediario_names, extract_intermediario_section, create_df_from_pdf
 import re
 import pdfplumber
 from collections import namedtuple
-#import pandas as pd
-#import numpy as np
 import matplotlib 
 import matplotlib.pyplot as plt
-#import os
 import seaborn as sns
 from datetime import datetime
 import locale
@@ -96,12 +93,12 @@ ratio_df = ratio_df.sort_values('Periodo_dt')
 ratio_df['Utilizzato_to_Accordato_Operativo_Ratio_Percentage'] = ratio_df['Utilizzato_to_Accordato_Operativo_Ratio'] * 100
 
 #with col1:
-#    st.subheader("Utilizzato to Accordato Operativo Ratio over Time")
-#    fig = px.bar(ratio_df, x = "Periodo_dt", y = "Utilizzato_to_Accordato_Operativo_Ratio_Percentage", template = "seaborn")
-#    st.plotly_chart(fig,use_container_width=True, height = 200)
+    st.subheader("Utilizzato to Accordato Operativo Ratio over Time")
+    fig = px.bar(ratio_df, x = "Periodo_dt", y = "Utilizzato_to_Accordato_Operativo_Ratio_Percentage", template = "seaborn")
+    st.plotly_chart(fig,use_container_width=True, height = 200)
 
 #with col2:
-#    st.subheader("Utilizzato per Intermediario")
-#    fig = px.pie(filtered_df, values = "Utilizzato", names = "Intermediario", hole = 0.5)
-#    fig.update_traces(text = filtered_df["Intermediario"], textposition = "outside")
-#    st.plotly_chart(fig,use_container_width=True)
+    st.subheader("Utilizzato per Intermediario")
+    fig = px.pie(filtered_df, values = "Utilizzato", names = "Intermediario", hole = 0.5)
+    fig.update_traces(text = filtered_df["Intermediario"], textposition = "outside")
+    st.plotly_chart(fig,use_container_width=True)
